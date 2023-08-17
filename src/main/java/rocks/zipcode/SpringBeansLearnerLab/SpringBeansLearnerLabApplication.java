8c417f52e3a5b0b4aa2e71ea02e20d461fb07f9e
Joe <Jhuntenburg@gmail.com>
2023-08-17 14:29:26 -0400

    started

---
+++ src/main/java/rocks/zipcode/SpringBeansLearnerLab/SpringBeansLearnerLabApplication.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.SpringBeansLearnerLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeansLearnerLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeansLearnerLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rocks/zipcode/SpringBeansLearnerLab/SpringBeansLearnerLabApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.SpringBeansLearnerLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeansLearnerLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
